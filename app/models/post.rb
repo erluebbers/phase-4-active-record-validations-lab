774ea28d362e414b1678cb9f62d20d6742db889b
Erik Luebbers <erluebbers@gmail.com>
2022-03-24 07:02:57 -0700

    commit tree 2e008c99f6b2019eff095b1c01c0079a97266d74

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,17 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, 
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+  validate :title_is_clickbait
+
+  private
+
+  def title_is_clickbait
+    if title.include? %w(Won't Believe Secret Top Guess)
+      errors.add(:title, "isn't clickbaity enough")
+    end
+  end
 
 
 end
